72d63a3a7954ca2afdffbe387476249f20e5b139
nao mori <lockn980@gmail.com>
2020-03-20 10:10:26 +0100

    intialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
 
-function App() {
+interface State { time: string, name: string, article: string, word: string }[]
+
+const App:React.FC = () => {
+  // useEffect(()=>{
+  //   axios.get("https://api.motimanager.com/artikle")
+  //   .then(data => console.log(data))
+  //   .catch(()=>setList([...list, {time: "3/20/2020", name:"Nao", article:"der", word: "Wind"}])
+  // },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
